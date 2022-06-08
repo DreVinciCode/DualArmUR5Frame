abec4aba776ae3ab9bc3f85c9ded0b6aab73a6ab
DreVinciCode <andre.cleaver@tufts.edu>
2022-06-08 18:25:05 -0400

    updated corrected arm joint rotation control

--- Assets/Scripts/AngleConversion.cs
+++ Assets/Scripts/AngleConversion.cs
@@ -5,63 +5,58 @@ namespace RosSharp.RosBridgeClient
 {
     public class AngleConversion : MonoBehaviour
     {
-
-        /*
-        * rt_shoulder_pan: 2.005
-        * rt_shoulder_lift: -2.10
-        * rt_elbow: -1.166
-        * rt_wrist_1: 3.14
-        * rt_wrist_2: -0.106
-        * rt_wrist_3: -1.836
-        * */
         public Transform Left_Shoulder_Pan;
-        public Transform Left_Shoulder_Tilt;
+        public Transform Left_Shoulder_Lift;
         public Transform Left_Elbow;
         public Transform Left_Wrist_1;
         public Transform Left_Wrist_2;
         public Transform Left_Wrist_3;
 
         public Transform Right_Shoulder_Pan;
-        public Transform Right_Shoulder_Tilt;
+        public Transform Right_Shoulder_Lift;
         public Transform Right_Elbow;
         public Transform Right_Wrist_1;
         public Transform Right_Wrist_2;
         public Transform Right_Wrist_3;
 
-        private void Start()
-        {
-            //Left_Shoulder_Pan.localEulerAngles = new Vector3(0, 0, RadianToDegree(-2.104f));
-            Left_Shoulder_Pan.localEulerAngles = UpdateArmOrientation(-1 * Vector3.forward, -2.104f);
-            Left_Shoulder_Tilt.localEulerAngles = UpdateArmOrientation( Vector3.up, -0.861f);
-            Left_Elbow.localEulerAngles = UpdateArmOrientation(-1* Vector3.up, 1.316f);
-            Left_Wrist_1.localEulerAngles = UpdateArmOrientation(-1* Vector3.up, -0.227f);
-            Left_Wrist_2.localEulerAngles = UpdateArmOrientation(-1 * Vector3.forward, 0);
-            Left_Wrist_3.localEulerAngles = UpdateArmOrientation(Vector3.up, -1.287f);
+        [Range(-6.28f, 6.28f)] public float Left_Shoulder_Pan_position;
+        [Range(-6.28f, 6.28f)] public float Left_Shoulder_Lift_position;
+        [Range(-3.14f, 3.14f)] public float Left_Elbow_position;
+        [Range(-6.28f, 6.28f)] public float Left_Wrist_1_position;
+        [Range(-6.28f, 6.28f)] public float Left_Wrist_2_position;
+        [Range(-6.28f, 6.28f)] public float Left_Wrist_3_position;
 
-            /*
-            Left_Shoulder_Tilt.localEulerAngles = new Vector3(0, RadianToDegree(-0.861f), 0);
-            Left_Elbow.localEulerAngles = new Vector3(0, RadianToDegree(1.316f), 0);
-            Left_Wrist_1.localEulerAngles = new Vector3(0,RadianToDegree(-0.227f), 0);
-            Left_Wrist_2.localEulerAngles = new Vector3(0, 0, RadianToDegree(0f));
-            Left_Wrist_3.localEulerAngles = new Vector3(0, RadianToDegree(-1.287f), 0);
-            */
+        [Range(-6.28f, 6.28f)] public float Right_Shoulder_Pan_position;
+        [Range(-6.28f, 6.28f)] public float Right_Shoulder_Lift_position;
+        [Range(-3.14f, 3.14f)] public float Right_Elbow_position;
+        [Range(-6.28f, 6.28f)] public float Right_Wrist_1_position;
+        [Range(-6.28f, 6.28f)] public float Right_Wrist_2_position;
+        [Range(-6.28f, 6.28f)] public float Right_Wrist_3_position;
 
+        //Setting Offset value for joint values 
+        private float _right_Shoulder_Pan_Offset_Position = (float)Math.PI;
+        private float _right_Shoulder_Lift_Offset_Position = (float)Math.PI/2;
+        private float _right_Elbow_Offset_Position = 0.0f;
+        private float _right_Wrist_1_Offset_Position = (float)Math.PI/2;
+        private float _right_Wrist_2_Offset_Position = 0.0f;
+        private float _right_Wrist_3_Offset_Position = -(float)Math.PI/4;
 
-            Right_Shoulder_Pan.localEulerAngles = UpdateArmOrientation(-1 * Vector3.forward, 2.005f);
-            Right_Shoulder_Tilt.InverseTransformDirection(UpdateArmOrientation(-1 * Vector3.up,(-1)* -2.104f));
-            //Right_Shoulder_Tilt.localEulerAngles = UpdateArmOrientation( Vector3.up, (-1) *-2.104f);
-            /*
-            Right_Shoulder_Tilt.localEulerAngles = new Vector3(0, RadianToDegree(-2.10f), 0);
-            Right_Elbow.localEulerAngles = new Vector3(0, RadianToDegree(-1.166f), 0);
-            Right_Wrist_1.localEulerAngles = new Vector3(0, RadianToDegree(3.14f), 0);
-            Right_Wrist_2.localEulerAngles = new Vector3(0, 0, RadianToDegree(-0.106f));
-            Right_Wrist_3.localEulerAngles = new Vector3(0, RadianToDegree(-1.836f), 0);
-            */
-        }
 
         private void Update()
         {
-
+            Right_Shoulder_Pan.localEulerAngles = UpdateArmOrientation(-1 * Vector3.forward, Right_Shoulder_Pan_position + _right_Shoulder_Pan_Offset_Position);
+            Right_Shoulder_Lift.localEulerAngles = UpdateArmOrientation(-1 * Vector3.up, Right_Shoulder_Lift_position + _right_Shoulder_Lift_Offset_Position);
+            Right_Elbow.localEulerAngles = UpdateArmOrientation(-1 * Vector3.up, Right_Elbow_position + _right_Elbow_Offset_Position);
+            Right_Wrist_1.localEulerAngles = UpdateArmOrientation(-1 * Vector3.up, Right_Wrist_1_position + _right_Wrist_1_Offset_Position);
+            Right_Wrist_2.localEulerAngles = UpdateArmOrientation(-1 * Vector3.forward, Right_Wrist_2_position + _right_Wrist_2_Offset_Position);
+            Right_Wrist_3.localEulerAngles = UpdateArmOrientation(-1 * Vector3.up, Right_Wrist_3_position + _right_Wrist_3_Offset_Position);
+            
+            Left_Shoulder_Pan.localEulerAngles = UpdateArmOrientation(-1 * Vector3.forward, Left_Shoulder_Pan_position);
+            Left_Shoulder_Lift.localEulerAngles = UpdateArmOrientation(Vector3.up, Left_Shoulder_Lift_position);
+            Left_Elbow.localEulerAngles = UpdateArmOrientation(-1 * Vector3.up, Left_Elbow_position);
+            Left_Wrist_1.localEulerAngles = UpdateArmOrientation(-1 * Vector3.up, Left_Wrist_1_position);
+            Left_Wrist_2.localEulerAngles = UpdateArmOrientation(-1 * Vector3.forward, Left_Wrist_2_position);
+            Left_Wrist_3.localEulerAngles = UpdateArmOrientation(Vector3.up, Left_Wrist_3_position);
         }
 
         private float RadianToDegree(float position)
